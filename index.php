e015eb902695a296e0fbedee7614839d003c5d70
JohnyCornflakes <anthony.brignone@gmail.com>
2018-06-22 22:34:51 +0200

    getParam_IsSetCheck

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
-<a href="bonjour.php?nom=Dupont&amp;prenom=Jean">Dis-moi bonjour !</a>
+<a href="page.php?nom=Cornflakes&amp;prenom=Johny&amp;repeter=3">Dis-moi bonjour !</a>
 
 <?php phpinfo(); ?>" > /var/www/html/index.php
--- page.php
+++ page.php
@@ -5,6 +5,28 @@
         <meta charset="utf-8" />
     </head>
     <body>
+	
+	<?php
+	if (isset($_GET['prenom']) AND isset($_GET['nom']) AND isset($_GET['repeter']))
+	{
+		// 1 : On force la conversion en nombre entier
+		$_GET['repeter'] = (int) $_GET['repeter'];
+
+		// 2 : Le nombre doit être compris entre 1 et 10
+		if ($_GET['repeter'] >= 1 AND $_GET['repeter'] <= 10) 
+		{	
+			for ($i = 0 ; $i < $_GET['repeter'] ; $i++)
+			{
+				echo 'Bonjour ' . $_GET['prenom'] . ' ' . $_GET['nom'] . ' !<br />';
+			}
+		}
+	}
+	else
+	{	
+   echo 'Bonjour';
+	}
+	?>
+	
         <h2>Affichage de texte avec PHP</h2>
         
         <p>
